522336f5392e1a424f47d4decffcc2c47b5be841
niom1 <63248775+niom1@users.noreply.github.com>
2021-05-28 08:59:40 +0200

    Add files via upload

---
+++ boilerplate-express-gomix/myApp.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname+"/public"));
+app.get("/",(req,res)=>res.sendFile(`${__dirname}/views/index.html`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
